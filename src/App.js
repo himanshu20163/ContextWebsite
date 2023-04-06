2509957ceb744058d1a98fc9d84ffaa229d4d028
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-06 21:27:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import A from './Components/A';
+// import A from './Components/ContextAPI_4/A';
+
+
+function App() {
+  return (
+    <div className="App">
+      <A/>
+    </div>
+  );
+}
+
+export default App;
